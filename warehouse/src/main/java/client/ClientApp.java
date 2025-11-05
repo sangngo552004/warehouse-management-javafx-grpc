fa29547866b6ac25dccbb8e7a810e0c33a7e2a93
tanvmt <vanminhtan04@gmail.com>
2025-11-05 22:31:59 +0700

    socket and login

--- warehouse/src/main/java/client/ClientApp.java
+++ warehouse/src/main/java/client/ClientApp.java
@@ -1,5 +1,31 @@
 package client;
 
-public class ClientApp {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import client.service.SocketService;
+
+public class ClientApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/client/view/Login.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Warehouse Management System - Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        primaryStage.setOnCloseRequest(e -> {
+            SocketService.getInstance().close();
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
